9c49f282881120ba6c57dbe2dadb516edb92ece2
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-07-12 22:01:12 +0530

    expense project Chart

--- Track-Your-Expenses/src/components/Chart/Chart.jsx
+++ Track-Your-Expenses/src/components/Chart/Chart.jsx
@@ -3,10 +3,22 @@ import "./Chart.css";
 import ChartBar from './ChartBar/ChartBar';
 
 
-const Chart = () => {
+const Chart = (props) => {
+  const dataPointValues=props.dataPoints.map(dataPoint=> dataPoint.value);
+  const totalMaximum=Math.max(...dataPointValues);
+
   return (
-    <div>Chart</div>
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint)=>( 
+      <ChartBar
+        key={dataPoint.id}
+        value={dataPoint.value}
+        maxValue={totalMaximum}
+        lable={dataPoint.lable}
+      />
+      ))}
+    </div>
   )
 }
 
-export default Chart
+export default Chart;
--- Track-Your-Expenses/src/components/Chart/ChartBar/ChartBar.jsx
+++ Track-Your-Expenses/src/components/Chart/ChartBar/ChartBar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./ChartBar.css";
 
-const ChartBar = () => {
+const ChartBar = (props) => {
+  let barFillFeight="0%";
+  
+  if(props.maxValue>0){
+    barFillFeight=Math.round((props.value / props.maxValue)*100)+"%";
+  }
+  
+  
   return (
-    <div>ChartBar</div>
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        <div className='chart-bar__fill' style={{height:barFillFeight}}></div>
+      </div>
+      <div className='chart-bar__lable'>{props.lable}</div>
+    </div>
   )
 }
 
