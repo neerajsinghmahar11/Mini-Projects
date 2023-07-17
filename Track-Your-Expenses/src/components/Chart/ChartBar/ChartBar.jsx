933318592f7306c11750e83569283626aa09a90a
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-07-17 11:19:18 +0530

    Chart

--- Track-Your-Expenses/src/components/Chart/ChartBar/ChartBar.jsx
+++ Track-Your-Expenses/src/components/Chart/ChartBar/ChartBar.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  let barFillFeight="0%";
-  
-  if(props.maxValue>0){
-    barFillFeight=Math.round((props.value / props.maxValue)*100)+"%";
+  let barFillHeight = '0%';
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
   
   
   return (
     <div className='chart-bar'>
       <div className='chart-bar__inner'>
-        <div className='chart-bar__fill' style={{height:barFillFeight}}></div>
+        <div className='chart-bar__fill' style={{height:barFillHeight}}></div>
       </div>
-      <div className='chart-bar__lable'>{props.lable}</div>
+      <div className='chart-bar__label'>{props.label}</div>
     </div>
   )
 }
 
-export default ChartBar
+export default ChartBar;
