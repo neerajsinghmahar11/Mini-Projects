99d9358968487d9a634b13472abb87d4725ebc0a
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-07-31 21:10:13 +0530

    MealList ,Card

--- food-ordering/src/App.js
+++ food-ordering/src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Layout/Header/Header";
 import { Fragment } from "react";
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
-    <Fragment>
-      <Header />
+    <Fragment >
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
