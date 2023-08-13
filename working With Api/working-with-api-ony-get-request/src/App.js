0b2a2fad8da98c96cfa73ce4e0f5c2fc1619cc1a
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-08-13 10:48:44 +0530

    working with firebase get and post request

---
+++ working With Api/working-with-api-ony-get-request/src/App.js
@@ -0,0 +1,71 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+ 
+  const getMoviesData = useCallback(async() => {
+    setIsLoading(true);
+    setError(null);
+
+   
+    try {
+      const response = await fetch(`https://swapi.dev/api/films/`);
+      
+      if(!response.ok){
+        throw new Error("something went wrong");
+      }
+
+      const data = await response.json();
+      
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crowl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  },[]);
+
+  useEffect(()=>{
+    getMoviesData()
+  },[getMoviesData])
+  
+
+  let content=<p>Found No Movies</p>;
+
+  if(movies.length>0){
+    content=<MoviesList movies={movies} />
+  }
+  if(error){
+    content=<p>{error}</p>;
+  }
+  if(isLoading){
+    content=<p>Loading...</p>
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={getMoviesData}>Fetch Movies</button>
+      </section>
+      <section>
+      {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
