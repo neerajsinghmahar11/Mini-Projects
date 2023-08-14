ce8fcea281186f0b95a1847554c1c694726b8cf8
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-08-14 22:01:57 +0530

    api post

--- working With Api/api-get-and-post/src/App.js
+++ working With Api/api-get-and-post/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,9 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
@@ -39,8 +39,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
+  async function addMovieHandler(movie) {
     console.log(movie);
+    const response = await fetch(
+      `https://react-using-api-f908d-default-rtdb.firebaseio.com/movies.json`,
+      {
+        method: "Post",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data=await response.json();
   }
 
   let content = <p>Found no movies.</p>;
