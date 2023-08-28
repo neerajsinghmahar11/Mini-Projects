412648a228463c02bc5e2a2a381d31b00fa937da
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2023-08-28 22:59:27 +0530

    handling errors

--- food-ordering/src/Components/Meals/AvailableMeals.jsx
+++ food-ordering/src/Components/Meals/AvailableMeals.jsx
@@ -6,59 +6,69 @@ import MealItem from './MealItem/MealItem';
 
 
 const AvailableMeals = () => {
-  const [meals,setMeals]=useState();
-  const [isLoading,setIsLoading]=useState(true);
-  const [httpError,setHttpError]=useState();
+  const [meals, setMeals] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
-useEffect(()=>{
+  useEffect(() => {
 
-  const getData=async()=>{
-    let mealsData=[];
-    const res=await fetch(`https://food-ordering-a6827-default-rtdb.firebaseio.com/meals.json`)
-    if(!res.ok){
-      throw new Error("Something went wrong!")
+    const getData = async () => {
+      let mealsData = [];
+      const res = await fetch(`https://food-ordering-a6827-default-rtdb.firebaseio.com/meals.json`)
+      if (!res.ok) {
+        throw new Error("Something went wrong!")
+      }
+
+      const data = await res.json();
+
+      for (let key in data) {
+        mealsData.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+      setMeals(mealsData);
+      setIsLoading(false);
     }
 
-    const data=await res.json();
-    
-    for(let key in data){
-      mealsData.push({
-        id:key,
-        name:data[key].name,
-        description:data[key].description,
-        price:data[key].price
-      })
-    }
-    setMeals(mealsData);
-    setIsLoading(false);
+
+    getData().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
+  }, [])
+
+
+  let mealsList = [];
+  if (isLoading) {
+    return mealsList = <section className={classes.isLoading}>
+      <h1>Loading...</h1>
+    </section>
+  } else {
+
+    mealsList = meals.map((meal) => (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />))
   }
-  getData()
-},[])
 
-
-let mealsList=[];
-if(isLoading){
-  return mealsList=<section className={classes.isLoading}>
-    <h1>Loading...</h1>
+  if(httpError){
+    return <section className={classes.mealsError}>
+    <h1>{httpError}</h1>
   </section>
-}else{
-
-  mealsList= meals.map((meal)=> (
-    <MealItem
-    key={meal.id}
-    id={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-    />))
   }
-
-
-    return (
+  return (
     <section className={classes.meals}>
-        <Card>
+      <Card>
         <ul>{mealsList}</ul>
-        </Card>
+      </Card>
     </section>
   )
 }
